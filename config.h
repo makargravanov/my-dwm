3f4deb0000bf3e031bb81a348f6bb8887d5067c0
alex <makargravanov@gmail.com>
2025-07-22 09:14:43 +0300

    test

--- config.h
+++ config.h
@@ -97,6 +97,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
+
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
